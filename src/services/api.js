8212281b584acede0a67ea826b057b5bcf87283f
Md Faizy <mdgmfaizy@gmail.com>
2024-04-30 14:09:48 +0530

    updated

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,4 @@
-
-const BASE_URL=import.meta.env.REACT_APP_API_URL;
+const BASE_URL = `${import.meta.env.REACT_APP_API_URL}/api/v1`;
 // const BASE_URL="http://localhost:3000/api/v1"
 
 // AUTH ENDPOINTS
@@ -8,5 +7,4 @@ export const endpoints = {
   LOGIN_API: BASE_URL + "/auth/login",
   VERIFY_USER: BASE_URL + "/auth/user/:id/:token",
   PROFILE_UPLOAD: BASE_URL + "/profile/profile",
-
-}
+};
